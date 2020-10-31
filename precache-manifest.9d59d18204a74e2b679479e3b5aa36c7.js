799667976e28e2ab503f8778309ba2d0eb63649c
Frederic Olivier <jpolivierr@gmail.com>
2020-10-30 23:48:53 -0400

    Updates

---
+++ precache-manifest.9d59d18204a74e2b679479e3b5aa36c7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476cd655f584c0b5eac559f1d2b6fad7",
+    "url": "/rp/index.html"
+  },
+  {
+    "revision": "66aef4189626e556661d",
+    "url": "/rp/static/css/main.b8ac11aa.chunk.css"
+  },
+  {
+    "revision": "47e60c921b58e701f2b4",
+    "url": "/rp/static/js/2.844046a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rp/static/js/2.844046a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66aef4189626e556661d",
+    "url": "/rp/static/js/main.5d2a0df6.chunk.js"
+  },
+  {
+    "revision": "f6fe3a6becab17a86e79",
+    "url": "/rp/static/js/runtime-main.dbeff65d.js"
+  },
+  {
+    "revision": "a16af3b4dac8209b15944c5aa737a04a",
+    "url": "/rp/static/media/Logo Edited.a16af3b4.png"
+  },
+  {
+    "revision": "300ff1c378d298662c5a8ffe839f2e57",
+    "url": "/rp/static/media/Main-house.300ff1c3.jpg"
+  },
+  {
+    "revision": "6af4af7fecc3b7f14224f47c9295c819",
+    "url": "/rp/static/media/business-1.6af4af7f.jpg"
+  },
+  {
+    "revision": "b30e30d6a65080ea2a482e0bbb1ee79b",
+    "url": "/rp/static/media/contractor.b30e30d6.jpg"
+  },
+  {
+    "revision": "cd5b82ff63c60c7b8be7e8972f675aec",
+    "url": "/rp/static/media/happy-asian.cd5b82ff.jpg"
+  },
+  {
+    "revision": "ecc10c41b9894591b0325b3b8e630850",
+    "url": "/rp/static/media/home-2.ecc10c41.jpg"
+  },
+  {
+    "revision": "26e4d2b1dbabc3ad1327be7224e09fdc",
+    "url": "/rp/static/media/house 2.26e4d2b1.jpg"
+  },
+  {
+    "revision": "7455f7b5a7933417d6afec5c9bd1724a",
+    "url": "/rp/static/media/house 3 edit.7455f7b5.png"
+  },
+  {
+    "revision": "105e1e983c715dbb06742ecaeae7bf25",
+    "url": "/rp/static/media/interior--1.105e1e98.jpg"
+  },
+  {
+    "revision": "83e2fb90f6ab4b72bb93d25d1b994dee",
+    "url": "/rp/static/media/interior-1.83e2fb90.jpg"
+  },
+  {
+    "revision": "637905880c602930fce335f9a55b3b2f",
+    "url": "/rp/static/media/invest-6.63790588.jpg"
+  },
+  {
+    "revision": "2e62e1fa77ffbaf68195451884fa9a92",
+    "url": "/rp/static/media/realtor.2e62e1fa.jpg"
+  },
+  {
+    "revision": "63ffd7a82a43d34750b8c3b144c83f4f",
+    "url": "/rp/static/media/sell-2.63ffd7a8.jpg"
+  },
+  {
+    "revision": "81cf41ef63f50cac6bb409303b4662fe",
+    "url": "/rp/static/media/white-background.81cf41ef.jpg"
+  },
+  {
+    "revision": "d9ecdc8f109226aca37a37f74c3e9d39",
+    "url": "/rp/static/media/woman-in-business.d9ecdc8f.jpg"
+  }
+]);
